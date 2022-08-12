7a4a7cafa5ac64c67e003e5fd8a9635afe4fcd02
LuanSantos00 <luancosmefilho@gmail.com>
2022-08-12 18:02:09 -0300

    first commit

---
+++ Introducao_a_Fila/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Introducao_a_Fila
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando a Fila
+            Queue<string> minhaFila = new Queue<string>();
+
+            //Adicionando na Fila
+            minhaFila.Enqueue("Primeiro Cliente");
+            minhaFila.Enqueue("Segundo Cliente");
+            minhaFila.Enqueue("Terceiro Cliente");
+            minhaFila.Enqueue("Quarto Cliente");
+            minhaFila.Enqueue("Quinto Cliente");
+
+            minhaFila.Dequeue();
+
+            foreach (string elemento in minhaFila)
+            {
+                Console.WriteLine(elemento);
+                minhaFila.Dequeue();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Introdução a Fila
+
+
+Iniciando os estudos na estrutura fila com dotnet/c#.
+
+<hr>
+
+Starting studies on the queue structure with dotnet/c#.
+
